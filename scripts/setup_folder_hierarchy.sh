7ef8afd42a39cbb8de065b2e435e4f1c56ceeb52
Florin Balint <florin.balint93@gmail.com>
2022-03-07 16:31:49 +0100

    Create a script for setting up the initial folder hierarchy.

---
+++ scripts/setup_folder_hierarchy.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+make_learn_folder() {
+    local LANGS="c cpp java go js ts python bash"
+    local FWORKS="spring angular react flutter"
+    
+
+    for dir in ${LANGS} ${FWORKS}
+    do
+        mkdir -p ${HOME}/Work/Learn/${dir}
+    done
+}
+
+make_learn_folder
+
+mkdir -p ${HOME}/Pictures
+mkdir -p ${HOME}/Work/repos
+mkdir -p ${HOME}/Work/logs
+mkdir -p ${HOME}/Work/kits
+mkdir -p ${HOME}/Work/scripts
+mkdir -p ${HOME}/Work/books
+
+mkdir -p ${HOME}/bills
+
